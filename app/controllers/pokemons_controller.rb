97a17db3c1b822c84c026b286f1f3bc3da14cb74
jessijoke <jessijoke@yahoo.com>
2021-03-14 00:42:15 -0500

    Teams and Pokemon MVC

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,32 @@
 class PokemonsController < ApplicationController
+    def index
+        @pokemon = Pokemon.all
+    end
+
+    def show
+        @pokemon = Pokemon.find(params[:id])
+    end
+
+    def new
+        @pokemon = Pokemon.new
+    end
+
+    def create
+        @pokemon = Pokemon.new
+        @pokemon.name = params[:name]
+        @pokemon.team_id = params[:team_id]
+        @pokemon.trainer_id = params[:trainer_id]
+        @pokemon.save
+        redirect_to pokemon_path(@pokemon)
+    end
+
+    def edit
+        @pokemon = Pokemon.find(params[:id])
+    end
+
+    def update
+        @pokemon = Pokemon.find(params[:id])
+        @pokemon.update(name: params[:pokemon][:name], trainer_id: params[:pokemon][:trainer_id], team_id: params[:pokemon][:team_id])
+        redirect_to pokemon_path(@pokemon)
+    end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -14,6 +14,7 @@ class TeamsController < ApplicationController
     def create
         @team = Team.new
         @team.name = params[:name]
+        @team.trainer_id = params
         @team.save
         redirect_to team_path(@team)
     end
@@ -24,7 +25,7 @@ class TeamsController < ApplicationController
 
     def update
         @team = Team.find(params[:id])
-        @team.update(name: params[:team][:name])
+        @team.update(name: params[:team][:name], trainer_id: params[:team][:trainer_id])
         redirect_to team_path(@team)
     end
 end
